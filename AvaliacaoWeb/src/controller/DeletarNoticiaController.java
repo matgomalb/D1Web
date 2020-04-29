aca3da052f797d52e7f79ae718d1075140daa0f6
johnathan.seara <johnathan.seara@BR-IT00142.globant.com>
2020-04-29 15:43:52 -0300

    Johnathan

--- AvaliacaoWeb/src/controller/DeletarNoticiaController.java
+++ AvaliacaoWeb/src/controller/DeletarNoticiaController.java
@@ -32,6 +32,7 @@ public class DeletarNoticiaController extends HttpServlet {
 		ns.excluir(noticia);
 		//ns.excluir(noticia);
 		
+		
 		PrintWriter out = response.getWriter();
 		out.print("Essa é a noticia: " + noticia.toString());
 		//ns.excluir(noticia);
